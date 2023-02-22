4c5b2886915dd30be5843b0b32715ce04d1350d1
Gloria Duncan Esi <esigloria32@gmail.com>
2023-02-22 19:57:38 +0100

    feat: update lists.c prototypes

--- shell.h
+++ shell.h
@@ -182,6 +182,8 @@ int renumber_history(info_t *info);
 list_t add_node(list_t *, const char *, int);
 list_t add_node_end(list_t *, const char *, int);
 size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
 
 /*==== parser.c ====*/
 int is_cmd(info_t *, char *);
