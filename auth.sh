5f0965fec6c3d4706dfe4e2d39ffc4bb95667012
CNwante <myphilothea@gmail.com>
2023-02-22 18:00:35 -0800

    file: remove auth.sh

--- auth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-{
-	cat <<-'EOH'
-	This file, contains the data of all individuals that contributed to the repository content.
-	EOH
-	echo
-	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
-} > AUTHORS
