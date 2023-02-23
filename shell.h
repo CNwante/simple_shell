af58794be501fe725c1eeeabd7eaa8d0baae41e5
CNwante <myphilothea@gmail.com>
2023-02-22 16:37:13 -0800

    refactor: rearanged the location of prototypes
    Caused code failure
    
    Errors fixed

--- shell.h
+++ shell.h
@@ -2,10 +2,10 @@
 #define _SIMPLE_SHELL_H_
 
 /*
- * ==== AUTHOR ====
- * Solomon Nwante
- * Gloria Duncan
- */
+* ==== AUTHOR ====
+* Solomon Nwante
+* Gloria Duncan
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -94,8 +94,8 @@ typedef struct passinfo
 	char **environ;
 	int env_changed;
 	int status;
-	/*==== pointer to cmd ; chain buffer, for memory mangement ===*/
-	char **cmd_buf;
+
+	char **cmd_buf; /*==== pointer to cmd ; chain buffer, for memory mangement ===*/
 	int cmd_buf_type; /*==== CMD_type ||, &&, ; ====*/
 	int readfd;
 	int histcount;
@@ -116,99 +116,26 @@ typedef struct builtin
 	int (*func)(info_t *);
 } builtin_table;
 
-/*==== atoi.c ====*/
-int interactive(info_t *);
-int is_delim(char, char *);
-int _isalpha(int);
-int _atoi(char *);
-
-/*==== builtin.c ====*/
-int _myexit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
-
-/*==== builtin1.c ====*/
-int _myhistory(info_t *);
-int _myalias(info_t *);
-
-/*==== environ.c ====*/
-char *_getenv(info_t *, const char *);
-int _myenv(info_t *);
-int _mysetenv(info_t *);
-int _myunsetenv(info_t *);
-int populate_env_list(info_t *);
-
-/*==== errors.c ====*/
-void _eputs(char *);
-int _eputchar(char);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
-
-/*==== errors1.c ====*/
-int _erratoi(char *);
-void print_error(info_t *, char *);
-int print_d(int, int);
-char *convert_number(long int, int, int);
-void remove_comments(char *);
-
-/*==== exits.c ====*/
-char *_strncpy(char *, char *, int);
-char *_strncat(char *, char *, int);
-char *_strchr(char *, char);
-
-/*==== getenv.c ====*/
-char **get_environ(info_t *);
-int _unsetenv(info_t *, char *);
-int _setenv(info_t *, char *, char *);
-
-/*==== getinfo.c ====*/
-void clear_info(info_t *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
-
-/*==== getline.c ====*/
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
-
-/*==== history.c ====*/
-char *get_history_file(info_t *info);
-int write_history(info_t *info);
-int read_history(info_t *info);
-int build_history_list(info_t *info, char *buf, int linecount);
-int renumber_history(info_t *info);
-
-/*==== lists.c ====*/
-list_t add_node(list_t *, const char *, int);
-list_t add_node_end(list_t *, const char *, int);
-size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t **, unsigned int);
-void free_list(list_t **);
-
-/*==== lists1.c ====*/
-size_t list_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
-
-/*==== memory.c ====*/
-int bfree(void **);
-
-/*==== parser.c ====*/
-int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
-
-/*==== realloc.c ====*/
-char *_memset(char *, char, unsigned int);
-void ffree(char **);
 
 /*=== shell_loop.c ====*/
 int hsh(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
+
+/*==== parser.c ====*/
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+
+/*==== loophsh.c ====*/
+int loophsh(char **);
+
+/*==== errors.c ====*/
+void _eputs(char *);
+int _eputchar(char);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
 
 /*==== string.c ====*/
 int _strlen(char *);
@@ -222,9 +149,87 @@ char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
 
+/*==== exits.c ====*/
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+
 /*==== tokenizer.c ====*/
 char **strtow(char *, char *);
 char **strtow2(char *, char);
+
+/*==== realloc.c ====*/
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+
+/*==== memory.c ====*/
+int bfree(void **);
+
+/*==== atoi.c ====*/
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+
+/*==== errors1.c ====*/
+int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+
+/*==== builtin.c ====*/
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+/*==== builtin1.c ====*/
+int _myhistory(info_t *);
+int _myalias(info_t *);
+
+/*==== getline.c ====*/
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
+
+/*==== getinfo.c ====*/
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+
+/*==== environ.c ====*/
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+
+/*==== getenv.c ====*/
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
+/*==== history.c ====*/
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+
+/*==== lists.c ====*/
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/*==== lists1.c ====*/
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
 
 /*==== vars.c ====*/
 int is_chain(info_t *, char *, size_t *);
